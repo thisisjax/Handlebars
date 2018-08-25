75c89336aa173e1691aae0edec935abb634e1dba
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-08-25 18:47:04 -0400

    initail setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('BBQ Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Black Bean Burger', true);
+INSERT INTO burgers (name) VALUES ('Avacado Burger');
