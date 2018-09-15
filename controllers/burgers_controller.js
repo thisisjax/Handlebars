539ac66c16e80363bea6ba85a3f507f8e42aec14
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-09-14 23:45:22 -0400

    final push

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,12 +18,12 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  buger.create([
+  burger.create([
     "burger_name", "devoured"
   ], [
     req.body.name, 0
   ], function(result) {
-    res.direct("/");
+    res.redirect("/");
   });
 });
 
@@ -42,17 +42,7 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-// router.delete("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
 
-//   burger.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
 
 
 // Export routes for server.js to use.
--- models/burger.js
+++ models/burger.js
@@ -19,11 +19,6 @@ var burger = {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
 };
 
 // Export the database functions for the controller (catsController.js).
